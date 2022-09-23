a7d64fe5080fa49a6d9ec7db9b9816b1fea98c00
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-23 14:28:49 +0200

    start writing puppeteer script

--- src/pages/api/BandcampData.ts
+++ src/pages/api/BandcampData.ts
@@ -1,16 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import chromium from "chrome-aws-lambda";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { config } from "../../config";
 
-type Data = {
-  name: string
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    const browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto(config.urlBandcamp);
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+    const allAlbums = await page.$$("#music-grid > li > a");
 
+    await allAlbums[0].click();
+    await page.waitForNavigation();
+    console.log(allAlbums[0]);
+    const shareButton = await page.$(".share-embed");
+    await shareButton?.click();
 
-  
-  res.status(200).json({ name: 'John Doe' })
+    const buttonEmbed = await page.$(".embed-other-services.panel-section > a");
+    // sizechoice small
+    await buttonEmbed?.click();
+
+    await browser.close();
+
+    res.status(200).json({ name: "John Doe", e: allAlbums });
 }
