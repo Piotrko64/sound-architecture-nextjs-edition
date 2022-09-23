51e110298846ceed69e44d07e8ccc64b6a2c031b
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-23 15:50:53 +0200

    state before fix problem with puppeteer

--- src/pages/api/BandcampData.ts
+++ src/pages/api/BandcampData.ts
@@ -3,29 +3,46 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { config } from "../../config";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    const iframes = [];
+
     const browser = await chromium.puppeteer.launch({
         args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
         defaultViewport: chromium.defaultViewport,
         executablePath: await chromium.executablePath,
-        headless: true,
+        headless: false,
         ignoreHTTPSErrors: true,
     });
     const page = await browser.newPage();
-    await page.goto(config.urlBandcamp);
 
+    await page.goto(config.urlBandcamp);
     const allAlbums = await page.$$("#music-grid > li > a");
 
-    await allAlbums[0].click();
+    await allAlbums[1].click();
     await page.waitForNavigation();
-    console.log(allAlbums[0]);
+
     const shareButton = await page.$(".share-embed");
     await shareButton?.click();
 
     const buttonEmbed = await page.$(".embed-other-services.panel-section > a");
-    // sizechoice small
     await buttonEmbed?.click();
 
-    await browser.close();
+    const smallSizeElement = await page.$(".sizechoice.small > .sizepreview");
+    await smallSizeElement?.click();
 
-    res.status(200).json({ name: "John Doe", e: allAlbums });
+    const inputElement = await page.$("input.embed_text");
+
+    const dataIframe = await page.evaluate((element) => element?.value, inputElement);
+
+    await iframes.push(dataIframe);
+
+    await page.goto(config.urlBandcamp);
+
+    const allAlbumss = await page.$$("#music-grid > li > a");
+    await allAlbumss[2].click();
+    await page.waitForNavigation();
+
+    await browser.close();
+    console.log(iframes);
+
+    res.status(200).json({ iframes });
 }
