7ca942077af0ef3d0b5c9ebecbfbd772dac2a005
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-29 14:12:59 +0200

    change folders place

--- src/components/SoundsPage/SoundsPageMainComponent.tsx
+++ src/components/SoundsPage/SoundsPageMainComponent.tsx
@@ -1,10 +1,17 @@
 import { iframeBandcampData } from "../../../@types/IframeBandcampDataInterface";
+import { useSearchingStore } from "../../store/useSearchingStore";
+import { filterByMusicTitle } from "../../utils/filterByMusicTitle";
 import { ListIframes } from "./listIframes/ListIframes";
+import { SearchSounds } from "./searchSounds/SearchSounds";
 
 export function SoundsPageMainComponent({ iframesListData }: { iframesListData: Array<iframeBandcampData> }) {
+    const { soundsTitleSearch } = useSearchingStore((state: any) => state);
+    const filterList = filterByMusicTitle(iframesListData, soundsTitleSearch);
+    console.log(filterList);
     return (
         <>
-            <ListIframes listIframesData={iframesListData} />
+            <SearchSounds />
+            <ListIframes listIframesData={filterList} />
         </>
     );
 }
