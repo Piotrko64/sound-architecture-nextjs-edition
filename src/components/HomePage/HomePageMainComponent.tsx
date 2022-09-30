15109a5035fb41b6dcca29bcc79bc1f9c927b98a
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-30 18:11:19 +0200

    check instagram photo on production

--- src/components/HomePage/HomePageMainComponent.tsx
+++ src/components/HomePage/HomePageMainComponent.tsx
@@ -1,9 +1,16 @@
+import { NextImage } from "../../ui/nextImage/NextImage";
 import { AboveMainFooter } from "./aboveMainFooter/AboveMainFooterComponent";
 
-export function HomePageMainComponent() {
+export function HomePageMainComponent({ instagramImagesUrl }: any) {
     return (
         <>
             <AboveMainFooter />
+            {JSON.stringify(instagramImagesUrl)}
+            {instagramImagesUrl.map((e: any) => (
+                // <NextImage key={e} imgURL={e} additionalClass="" />
+                <img src={e} key={e} height="50" width="100" />
+            ))}
+            {/* <NextImage imgURL={instagramImagesUrl} additionalClass=""/> */}
         </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,29 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import axios from "axios";
-import { InstagramUserInterface } from "../../@types/SSG/InstagramUserInterface";
-import { HomePageMainComponent } from "../components/HomePage/HomePageMainComponent";
 
-const Home: NextPage = ({ posts }: any) => {
-    return <HomePageMainComponent />;
+import { HomePageMainComponent } from "../components/HomePage/HomePageMainComponent";
+import { exampleInstagramData } from "../data/exampleInstagramData";
+import { InstagramUserInterface } from "../@types/SSG/InstagramUserInterface";
+
+const Home: NextPage = ({ instagramImagesUrl }: any) => {
+    return <HomePageMainComponent instagramImagesUrl={instagramImagesUrl} />;
 };
 
 export default Home;
 export async function getStaticProps() {
-    // const dataPosts: InstagramUserInterface = await axios({
-    //     url: "https://api.zenrows.com/v1/?apikey=f0d260bdf121721ace1991d288f25add9b0ccd29&url=https%3A%2F%2Fwww.instagram.com%2Fsoundarchitecture_sa%2F&js_render=true&premium_proxy=true&autoparse=true",
-    //     method: "GET",
-    // })
-    //     .then((response) => response.data)
-    //     .catch((error) => console.log(error));
+    const dataPosts: InstagramUserInterface = await axios({
+        url: "https://api.zenrows.com/v1/?apikey=f0d260bdf121721ace1991d288f25add9b0ccd29&url=https%3A%2F%2Fwww.instagram.com%2Fsoundarchitecture_sa%2F&js_render=true&premium_proxy=true&autoparse=true",
+        method: "GET",
+    })
+        .then((response) => response.data)
+        .catch(() => exampleInstagramData);
 
     return {
         props: {
-            // imagesUrl: dataPosts.user.edge_owner_to_timeline_media.edges.map((post) => post.node.display_url),
+            instagramImagesUrl: dataPosts.user.edge_owner_to_timeline_media.edges.map(
+                (post) => post.node.display_url
+            ),
         },
     };
 }
