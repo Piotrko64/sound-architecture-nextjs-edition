6ae9a5de3b8836adf103fd69cccebe241ff5e2c1
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-07 10:50:23 +0200

    check vercel deploy

--- src/utils/SSG/bandcamp/getBandcampDataForIframe.tsx
+++ src/utils/SSG/bandcamp/getBandcampDataForIframe.tsx
@@ -5,55 +5,55 @@ import { websitesUrl } from "../../../config/websitesUrl";
 
 export async function getBandcampDataDataForIframe() {
     const iframes: ArrayIframesBandcamp = [];
-    // const { urlBandcamp } = websitesUrl;
+    const { urlBandcamp } = websitesUrl;
 
-    // const browser = await chromium.puppeteer.launch(await puppeteerConfig());
-    // const page = await browser.newPage();
+    const browser = await chromium.puppeteer.launch(await puppeteerConfig());
+    const page = await browser.newPage();
 
-    // await page.goto(urlBandcamp);
+    await page.goto(urlBandcamp);
 
-    // const allAlbumsUser = await page.$$("#music-grid > li > a");
+    const allAlbumsUser = await page.$$("#music-grid > li > a");
 
-    // for (let i = 0; i <= 1; i++) {
-    //     const allAlbums = await page.$$("#music-grid > li > a");
-    //     await allAlbums[i].click();
-    //     await page.waitForNavigation();
+    for (let i = 0; i <= 1; i++) {
+        const allAlbums = await page.$$("#music-grid > li > a");
+        await allAlbums[i].click();
+        await page.waitForNavigation();
 
-    //     const tracks = await page.$$("td > .title > a");
-    //     const tracksLength = tracks.length;
+        const tracks = await page.$$("td > .title > a");
+        const tracksLength = tracks.length;
 
-    //     const titlesAllTracks: Array<string> = [];
-    //     for (let i = 0; i <= 1; i++) {
-    //         const tracks = await page.$$("td > .title > a > .track-title");
-    //         tracks.forEach(async (track) => {
-    //             const trackTitle = await page.evaluate((element) => {
-    //                 return element?.innerText;
-    //             }, track);
-    //             titlesAllTracks.push(trackTitle);
-    //         });
-    //     }
+        const titlesAllTracks: Array<string> = [];
+        for (let i = 0; i <= 1; i++) {
+            const tracks = await page.$$("td > .title > a > .track-title");
+            tracks.forEach(async (track) => {
+                const trackTitle = await page.evaluate((element) => {
+                    return element?.innerText;
+                }, track);
+                titlesAllTracks.push(trackTitle);
+            });
+        }
 
-    //     const shareButton = await page.$(".share-embed");
-    //     await shareButton?.click();
+        const shareButton = await page.$(".share-embed");
+        await shareButton?.click();
 
-    //     const buttonEmbed = await page.$(".embed-other-services.panel-section  > a");
-    //     await buttonEmbed?.click();
+        const buttonEmbed = await page.$(".embed-other-services.panel-section  > a");
+        await buttonEmbed?.click();
 
-    //     const bigSizeElement = await page.$(".sizechoice.large > .sizepreview");
-    //     await bigSizeElement?.click();
+        const bigSizeElement = await page.$(".sizechoice.large > .sizepreview");
+        await bigSizeElement?.click();
 
-    //     const inputElement = await page.$("input.embed_text");
+        const inputElement = await page.$("input.embed_text");
 
-    //     const dataIframe = await page.evaluate((element) => {
-    //         return element?.value;
-    //     }, inputElement);
+        const dataIframe = await page.evaluate((element) => {
+            return element?.value;
+        }, inputElement);
 
-    //     iframes.push({ iframeLink: dataIframe, mainTitles: titlesAllTracks });
+        iframes.push({ iframeLink: dataIframe, mainTitles: titlesAllTracks });
 
-    //     await page.goto(urlBandcamp);
-    // }
+        await page.goto(urlBandcamp);
+    }
 
-    // await browser.close();
+    await browser.close();
 
     return iframes;
 }
