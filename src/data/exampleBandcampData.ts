6fc6205a9b0b6813a66af5e102c8cb62fecab17b
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-09 15:14:56 +0200

    add exampleBandcamp data

---
+++ src/data/exampleBandcampData.ts
@@ -0,0 +1,87 @@
+export const exampleBandcampData = [
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 786px;" src="https://bandcamp.com/EmbeddedPlayer/album=3918256789/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/sailing-ambience">Sailing Ambience by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "AMBNaut, Sea, Water, Waves, Yacht, Engine, Yacht Swaying, Daytime, SA, ZOOM H1n, 9624",
+            "AMBNaut, Wind, Soft Waves, Cloth Clapping, Background Engine, SA, ZOOM H4n pro, 9624",
+            "AMBNaut, Sailing, Wind, Waves, Background Engine, SA, ZOOM H4n pro, 9624",
+            "AMBNaut, Sailing, Wind, Waves, Background Engine, SA, ZOOM H4n pro, 9624 [2]",
+            "AMBNaut, Sailing, Engine Sound, Steering Wheel, SA, ZOOM H1n, 9624",
+            "AMBNaut, Anchored, Waves, Wave Splash, SA, ZOOM H4n pro, 9624",
+            "AMBNaut, Anchored, Waves, Yacht Splash, SA, ZOOM H4n pro, 9624",
+            "AMBNaut, Sailing, Waves, Background Engine, SA, Rode VideoMicro, 9624",
+            "AMBNaut, Sailing, Waves, Background Engine, SA, Rode VideoMicro, 9624 [2]",
+            "AMBNaut, Sailing, Waves, Background Engine, SA, Rode VideoMicro, 9624 [3]",
+            "AMBNaut, Sailing, Wind, Waves, Background Engine, SA, ZOOM H4n pro, 9624",
+            "AMBNaut, Sailing, Wind, Waves, Background Engine, SA, ZOOM H4n pro, 9624 [2]",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album=2587588493/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/porto-katsiki-ambience">Porto Katsiki Ambience by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "AMBSea, Distant Cicadas Sound, Sea Waves, Pebbly Beach, Birds, Daytime, Porto Katsiki, Greece, SA, ZOOM H4n pro, 9624",
+            "AMBSea, Distant Cicadas Sound, Sea Waves, Pebbly Beach, Birds Daytime, Porto Katsiki, Greece, SA, ZOOM H4n pro, 9624 [2]",
+            "AMBSea, Distant Cicadas Sound, Sea Waves, Pebbly Beach, Birds Daytime, Porto Katsiki, Greece, SA, ZOOM H4n pro, 9624 [3]",
+            "AMBSea, Distant Cicadas Sound, Sea Waves, Pebbly Beach, Waves Crushing, Daytime, Porto Katsiki, Greece, SA, ZOOM H4n pro, 9624",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 687px;" src="https://bandcamp.com/EmbeddedPlayer/album=1551662299/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/krka-waterfall">Krka Waterfall by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, ZOOM H1n, 9624",
+            "WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, ZOOM H1n, 9624 [2]",
+            "WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, ZOOM H1n, 9624 [3]",
+            "WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, Rode VideoMicro, 9624",
+            "waterfall, Waterfalls, nature, National Park, water, SA, free, foley, - WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, Rode VideoMicro, 9624 [2]",
+            "waterfall, Waterfalls, nature, National Park, water, SA, free, foley, - WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, Rode VideoMicro, 9624 [3]",
+            "waterfall, Waterfalls, nature, National Park, water, SA, free, foley, watrfall - WATRFall, Waterfall Sound, Krka National Park, Croatia, SA, Rode VideoMicro, 9624 [4]",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 753px;" src="https://bandcamp.com/EmbeddedPlayer/album=2502325091/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/swish-sound-effects">Swish - Sound Effects by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "Video Edit Swishes, SA, 9624",
+            "SWSH, Big Swish, Woosh, SA, 9624",
+            "SWSH, Fast Swish, SA, 9624",
+            "SWSH, Swish, Fast Swish, SA, 9624",
+            "SWSH, Swish, SA, 9624",
+            "SWSH, Swish, SA, 9624, [2]",
+            "SWSH, Swish, SA, 9624, [3]",
+            "SWSH, Swish, SA, 9624, [4]",
+            "SWSH, Swish, SA, 9624, [5]",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album=714654310/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/air-conditioner">Air Conditioner by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "MACHHvac, Air Conditioner, Fan Engine, Wind Blow, SA, ZOOM H1n, 9624",
+            "MACHHvac, Air Conditioner, Fan Engine, Wind Blow, SA, ZOOM H1n, 9624, [2]",
+            "MACHHvac, Air Conditioner, Fan Engine, Wind Blow, SA, ZOOM H1n, 9624, [3]",
+            "MACHHvac, Fan Engine, Fan, Wind Blow, Air Conditioning, SA, ZOOM H1n, 9624",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album=1887217169/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/countryside-ambience">Countryside Ambience by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "AMBRurl, Countryside, Birds, Strong Wind, Pheasant, Dog Barking, Background Traffic, Daytime, Poland, SA, ZOOM H1n, 9624",
+            "AMBRurl, Countryside, Birds, Strong Wind, Pheasant, Dog Barking, Background Traffic, Daytime, Poland, SA, ZOOM H1n, 9624, [2]",
+            "AMBRurl, Countryside, Birds, Strong Wind, Pheasant, Dog Barking, Background Traffic, Daytime, Poland, SA, ZOOM H1n, 9624, [3]",
+            "AMBRurl, Countryside, Birds, Strong Wind, Pheasant, Daytime, Poland, SA, ZOOM H1n, 9624",
+        ],
+    },
+    {
+        iframeLink:
+            '<iframe style="border: 0; width: 350px; height: 555px;" src="https://bandcamp.com/EmbeddedPlayer/album=2608927152/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="https://soundarchitecture.bandcamp.com/album/windy-marina-ambience">Windy Marina Ambience by Sound Architecture</a></iframe>',
+        mainTitles: [
+            "AMBSea, Harbour, Strong Wind, Gusty Wind, Rain, Halyard Swaying, Rope Sound, Ringing Metal, Water, Waves, Daytime, Croatia, Trogir, SA, ZOOM H1n, 9624",
+            "AMBSea, Harbour, Strong Wind, Gusty Wind, Rain, Halyard Swaying, Rope Sound, Ringing Metal, Water, Waves, People Talking Daytime, Croatia, Trogir, SA, ZOOM H1n, 9624.WAV",
+            "AMBSea, Harbour, Strong Wind, Gusty Wind, Rain, People Shouting, Halyard Swaying, Rope Sound, Ringing Metal, Waves, Daytime, Croatia, Trogir, SA, ZOOM H1n, 9624.WAV",
+        ],
+    },
+];
--- src/utils/SSG/bandcamp/getBandcampDataForIframe.tsx
+++ src/utils/SSG/bandcamp/getBandcampDataForIframe.tsx
@@ -1,9 +1,10 @@
 import { websitesUrl } from "../../../config/websitesUrl";
+import { exampleBandcampData } from "../../../data/exampleBandcampData";
 
 export async function getBandcampDataDataForIframe() {
     const iframes = await fetch(websitesUrl.urlToFetchDataToIframe)
         .then((data) => data.json())
         .catch((err) => console.log(err));
 
-    return iframes;
+    return iframes || exampleBandcampData;
 }
